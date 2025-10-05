1d357fdba5a2c3661dc07c5260541a3e7aa4b066
sriyavedasolarenergies <sriyavedasolarenergies@gmail.com>
2025-10-05 21:40:42 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react"; // or whatever you use
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  base: "/sriyavedasolarenergies/", // 👈 VERY IMPORTANT
 });
